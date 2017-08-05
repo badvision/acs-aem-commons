31e3bddd18677b5d702d7208292d3de1d87e75a2
Brendan Robert <brobert@adobe.com>
2017-08-04 20:36:49 -0500

    Added all form field definitions and list parsing logic

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/SetProperty.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/SetProperty.java
@@ -17,7 +17,13 @@ package com.adobe.acs.commons.mcp.impl.processes;
 
 import com.adobe.acs.commons.mcp.ProcessDefinition;
 import com.adobe.acs.commons.mcp.ProcessInstance;
+import com.adobe.acs.commons.mcp.form.FormField;
+import com.adobe.acs.commons.mcp.form.PathfieldComponent;
+import com.adobe.acs.commons.mcp.form.RadioComponent;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.jcr.RepositoryException;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.PersistenceException;
@@ -27,41 +33,99 @@ import org.apache.sling.api.resource.ResourceResolver;
  * Set a missing property on target nodes
  */
 public class SetProperty implements ProcessDefinition {
-    private static enum SetRule{ALWAYS_SET, SET_IF_MISSING, ALWAYS_APPEND, APPEND_IF_MISSING}
-    
-    private static enum Plurality{SINGLE_VALUE, LIST}
-    
-    private static enum PropertyType{STRING, LONG, DOUBLE, BOOLEAN, DATE}
-    
+    public static final String DEFAULT_TREE_NODE_TYPES = "sling:folder,sling:orderedfolder,nt:hierarchynode,nt:unstructured,cq:page";
+
+    public static final String DEFAULT_NODE_TYPES = "nt:unstructured,cq:pagecontent,dam:asset";
+
+    private static enum SetRule {
+        SET_IF_MISSING, ALWAYS_SET, ALWAYS_APPEND, APPEND_IF_MISSING
+    }
+
+    private static enum Plurality {
+        SINGLE, LIST
+    }
+
+    private static enum PropertyType {
+        STRING, LONG, DOUBLE, BOOLEAN, DATE
+    }
+
+    @FormField(
+            name = "Base Path",
+            description = "Starting point of process",
+            hint = "/content/dam/my-folder, /content/my-site/my-page, etc.",
+            component = PathfieldComponent.class
+    )
     private String basePath;
-    
-    private String traverseTypes;
+
+    @FormField(
+            name = "Tree types",
+            description = "Case-insensitive node types that are interpreted as tree nodes comma-separated, * = all",
+            options = {"default="+DEFAULT_TREE_NODE_TYPES}
+            
+    )
+    private String traverseTypes=DEFAULT_TREE_NODE_TYPES;
     private List<String> traverseTypeList;
-    
-    private String targetNodeTypes;
+
+    @FormField(
+            name = "Node types",
+            description = "Only consider properties on these node types, * = all",
+            options = {"default="+DEFAULT_NODE_TYPES}
+            
+    )
+    private String targetNodeTypes = DEFAULT_NODE_TYPES;
     private List<String> targetNodeTypeList;
-    
+
+    @FormField(
+            name = "Property path",
+            description = "Relative to target node, can be a path",
+            hint = "myProperty, jcr:content/metadata/someProperty"
+    )
     private String relativePropertyPath;
-    
+
+    @FormField(
+            name = "Property type",
+            description = "Property type (usually string)",
+            component = RadioComponent.EnumerationSelector.class,
+            options = "default=STRING"
+    )
     private PropertyType propertyType;
-    
+
+    @FormField(
+            name = "Property multi",
+            description = "Single or multiple values in a list?",
+            component = RadioComponent.EnumerationSelector.class,
+            options = "default=SINGLE"
+    )
     private Plurality propertyPlurality;
-    
+
+    @FormField(
+            name = "Property value",
+            description = "Property value set according to the specified rule, value must match selected type",
+            hint = "12345, myTag:someTag, Dates can use NOW, NOW-1 (yesterday), etc."
+    )
     private String propertyValue;
-    
+
+    @FormField(
+            name = "Set rule",
+            description = "Rule to decide when property should be set and how to set it",
+            component = RadioComponent.EnumerationSelector.class,
+            options = "default=SET_IF_MISSING"
+    )
     private SetRule setRule;
 
     @Override
     public String getName() {
         return "Set Property";
     }
-    
+
     @Override
     public void init() throws RepositoryException {
-        if (propertyPlurality == Plurality.SINGLE_VALUE && (
-                setRule == SetRule.ALWAYS_APPEND || setRule == SetRule.APPEND_IF_MISSING)) {
+        if (propertyPlurality == Plurality.SINGLE && (setRule == SetRule.ALWAYS_APPEND || setRule == SetRule.APPEND_IF_MISSING)) {
             throw new RepositoryException("Selected set rule " + setRule.name() + " only applies for list properties");
         }
+        traverseTypeList = parseList(traverseTypes);
+        targetNodeTypeList = parseList(targetNodeTypes);
+        
     }
 
     @Override
@@ -70,5 +134,13 @@ public class SetProperty implements ProcessDefinition {
 
     @Override
     public void storeReport(ProcessInstance instance, ResourceResolver rr) throws RepositoryException, PersistenceException {
-    }    
+    }
+    
+    private List<String> parseList(String values) {
+        if (values == null || values.isEmpty() || values.contains("*")) {
+            return Collections.EMPTY_LIST;
+        } else {
+            return Stream.of(values.split(",")).map(String::trim).filter(s->!s.isEmpty()).collect(Collectors.toList());
+        }
+    }
 }
